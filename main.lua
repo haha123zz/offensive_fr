c21ba8c0ae9e6345efb71f8c8c743d29e8ced0f2
hahahahz1 <newdisc123@protonmail.com>
2022-01-10 05:18:31 -0500

    z

---
+++ main.lua
@@ -0,0 +1,9 @@
+local function load(path)
+    if (typeof(path) == "string") then
+        local success, code = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/haha123zz/offensive_fr/master/"..path..".lua")
+
+        if (success) then
+            return loadstring(code)()
+        end
+    end
+end
