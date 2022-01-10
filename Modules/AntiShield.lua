af8bd2be165869ce5d1a6bda4ab17ff7feccd55e
hahahahz1 <newdisc123@protonmail.com>
2022-01-10 05:32:23 -0500

    xd

--- Modules/AntiShield.lua
+++ Modules/AntiShield.lua
@@ -4,3 +4,12 @@ AntiShield.On = false
 function AntiShield:Toggle(val)
     AntiShield.On = val or not AntiShield.On
 end
+
+local oldIndex 
+oldIndex = hookmetamethod(game, "__index", function(i, v)
+    if (tostring(i) == "Blocking" and v == "Value" and AntiShield.On) then
+        return false 
+    end
+
+    return oldIndex(i, v)
+end)
--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 local function load(path)
     if (typeof(path) == "string") then
-        local success, code = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/haha123zz/offensive_fr/master/"..path..".lua")
+        local success, code = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/haha123zz/offensive_fr/main/"..path..".lua")
 
         if (success) then
             return loadstring(code)()
@@ -8,4 +8,17 @@ local function load(path)
     end
 end
 
+local success, LinoriaLibrary = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/UI%20Library%202.0.lua")
+if (success) then
+    getgenv().LinoriaLibrary = LinoriaLibrary
+    local AntiShield = load("Modules/AntiShield")
 
+    local Window = LinoriaLibrary:CreateWindow("Offensive Rewrite")
+    local CombatTab = Window:AddTab("Combat")
+    local ShieldTabbox = CombatTab:AddLeftTabbox("Shield")
+    local ShieldBlatant = ShieldTabbox:AddTab("Legit")
+
+    ShieldBlatant:AddToggle('Anti shield', {Text = "Anti shield", Default = AntiShield.On}):OnChanged(function(val)
+        print(val)
+    end)
+end
