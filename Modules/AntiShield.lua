e40ddb85c54f2e526b9d8be400398d3f7977087a
hahahahz1 <newdisc123@protonmail.com>
2022-01-10 05:32:31 -0500

    xd

--- Modules/AntiShield.lua
+++ Modules/AntiShield.lua
@@ -8,6 +8,7 @@ end
 local oldIndex 
 oldIndex = hookmetamethod(game, "__index", function(i, v)
     if (tostring(i) == "Blocking" and v == "Value" and AntiShield.On) then
+        getgenv().LinoriaLibrary:Notify('Spoofing '..i.Parent.Name..' shield.')
         return false 
     end
 
